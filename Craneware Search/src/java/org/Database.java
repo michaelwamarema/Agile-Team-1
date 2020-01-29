9389b90979701c9dd2dfdea06c212e6682018c17
jodielaurenson <jodielaurenson@labpc042.computing.dundee.ac.uk>
2020-01-29 15:19:03 +0000

    Class for a single record

--- Craneware Search/src/java/org/Database.java
+++ Craneware Search/src/java/org/Database.java
@@ -25,29 +25,38 @@ public class Database {
     public Connection setUpConnection(){
         Connection con;
         try {  
-            Class.forName("com.mysql.jdbc.Driver");
+           Class.forName("com.mysql.jdbc.Driver");
         
-            
            con = DriverManager.getConnection("jdbc:mysql://silva.computing.dundee.ac.uk:3306/19agileteam1db","19agileteam1","8760.at1.0678"); //connection details
-           
-           
-           
-           Statement stmt = con.createStatement();
-           ResultSet result = stmt.executeQuery("SELECT * FROM provider WHERE providerState = 'FL'"); //the query being executed, selects all results in florida
-           
-           getResult(result);
            
            return con;
            
         } catch (ClassNotFoundException ex) {
-            System.out.println("Error1");
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
         catch (SQLException ex) {
-            System.out.println("Error2");
-                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+    
+    /**
+     * ********Runs a simple search, not needed in final version
+     * 
+     * @param con
+     * @return 
+     */
+    public ResultSet runSelect(Connection con){
+        ResultSet result = null;
+        try{
+            Statement stmt = con.createStatement();
+            result = stmt.executeQuery("SELECT * FROM provider WHERE providerZipCode = 36301"); //the query being executed, selects all results in florida
+
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
     
     /**
@@ -67,7 +76,100 @@ public class Database {
     }
     
     
+    //-------------STORED PROCEDURE METHODS-------------\\
     
+    
+    /**
+     * This method runs a stored procedure to search a city and returns the results 
+     * 
+     * @param city The city the user wants to look for
+     * @param con The connection to the db server
+     * @return the results of the query
+     */
+    public ResultSet runSearchCityP(String city, Connection con){
+        try {
+            CallableStatement stmt;
+            
+            stmt = con.prepareCall("{CALL search_city(?)}");
+            stmt.setString(1, city);
+            
+            ResultSet result = stmt.executeQuery();
+        
+        
+            return result;
+            } catch (SQLException ex) {
+                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }  
+    }
+    
+    /**
+     * This method runs a stored procedure to search an address and returns the results 
+     * 
+     * @param address The address the user wants to look for
+     * @param con The connection to the db server
+     * @return the results of the query
+     */
+    public ResultSet runSearchAddressP(String address, Connection con){
+        try {
+            CallableStatement stmt;
+            
+            stmt = con.prepareCall("{CALL search_address(?)}");
+            stmt.setString(1, address);
+            
+            ResultSet result = stmt.executeQuery();
+        
+        
+            return result;
+            } catch (SQLException ex) {
+                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }  
+    }
+    
+    /**
+     * This method runs a stored procedure to search an address and returns the results 
+     * 
+     * @param address The address the user wants to look for
+     * @param con The connection to the db server
+     * @return the results of the query
+     */
+    public ResultSet runSearchConditionP(String condition, Connection con){
+        try {
+            CallableStatement stmt;
+            
+            stmt = con.prepareCall("{CALL search_by_procedure(?)}");
+            stmt.setString(1, condition);
+            
+            ResultSet result = stmt.executeQuery();
+        
+        
+            return result;
+            } catch (SQLException ex) {
+                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }  
+    }
+    
+    public ResultSet runRestrictPriceP(double min, double max, Connection con){
+        try {
+            CallableStatement stmt;
+            
+            stmt = con.prepareCall("{CALL restrict_price(?,?)}");
+            stmt.setDouble(1, min);
+            stmt.setDouble(2, max);
+            
+            ResultSet result = stmt.executeQuery();
+        
+        
+            return result;
+            } catch (SQLException ex) {
+                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }  
+    }
     
     
 }
+
+
